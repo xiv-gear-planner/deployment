03b2eeb205e7aa82075d32c44a322ec37fe29483
XP <smurfs@smurfs.verymad.net>
2025-02-16 21:31:41 -0800

    Updates

--- preview.sh
+++ preview.sh
@@ -4,4 +4,4 @@ set -e
 
 cd xivgear-chart
 
-helm dependency update && helm diff upgrade xivgear . -f ./values.yaml --dry-run=server
+helm dependency update && helm diff upgrade xivgear . -f ./values.yaml --dry-run=server --debug
